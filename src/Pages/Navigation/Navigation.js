3c5d99d6d7a5a7b46abbd4b9b146a7151681c3e5
Azwad-Ishraq <azwadishraq@.com>
2021-11-19 20:31:46 +0600

    commit

--- src/Pages/Navigation/Navigation.js
+++ src/Pages/Navigation/Navigation.js
@@ -3,35 +3,26 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import './Navigation.css'
-import {  NavLink } from 'react-router-dom';
-import useAuth from '../../hooks/useAuth';
+
+
 import { useState } from 'react';
-import { Drawer, SwipeableDrawer } from '@mui/material';
+import {  SwipeableDrawer } from '@mui/material';
 import DrawerItems from './DrawerItems/DrawerItems';
 const Navigation = () => {
   const [openDrawer,setOpenDrawer] = useState(false)
-  const {user,logOut} = useAuth()
-  const linkStyle = {
-    textDecoration: 'none',
-    marginLeft: '10px',
-   
-    color:'black'
-  }
-  const activeLinkStyle = {
-    backgroundColor: '#12d0d9',
-    color:'white',
-    borderRadius: '3px'
-  }
+ 
  
     return (
         <Box style={{width:'100%'}} sx={{ flexGrow: 1 }}>
       <AppBar style={{backgroundColor: 'transparent',color:'black',boxShadow:'0px 0px'}} position="static">
         <Toolbar>
           <IconButton
+            style={{position:'fixed'}}
+          
             size="large"
             edge="start"
             color="inherit"
